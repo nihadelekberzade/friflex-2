4f0c20760ad165a58de9e2172bf576b8b88e0cea
neehad <elekberzadeh333@gmail.com>
2021-08-26 06:47:52 +0400

    header dropdown overhaul,Diksi page added,project cards changed and etc..

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,18 @@
 const mobileMenu = document.querySelector('.header__menu-mobile');
 const body = document.querySelector('body');
 document.querySelector('.header__menu-btn').addEventListener('click', () => {
-	body.classList.toggle('body--fixed');
-	mobileMenu.classList.toggle('header__menu-mobile--hidden');
+  body.classList.toggle('body--fixed');
+  mobileMenu.classList.toggle('header__menu-mobile--hidden');
+})
+document.querySelectorAll('.dropdown').forEach(el => {
+  el.addEventListener('mouseover', () => {
+    el.querySelector('.dropdown__content').style.height = `${el.querySelector('.dropdown__content-inner').offsetHeight.toString()}px`;
+    el.querySelector('.dropdown__content').style.opacity = '1';
+    el.querySelector('.dropdown__content').style.visibility = 'visible';
+  })
+  el.addEventListener('mouseleave', () => {
+    el.querySelector('.dropdown__content').style.height = '0';
+    el.querySelector('.dropdown__content').style.opacity = '0';
+    el.querySelector('.dropdown__content').style.visibility = 'hidden';
+  })
 })
